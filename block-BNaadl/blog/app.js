e0647485d66defc323612afabc75cf7a20abafbc
upendra <upendrarajput9911@gmail.com>
2022-03-31 13:13:40 +0530

    done

--- block-BNaadl/blog/app.js
+++ block-BNaadl/blog/app.js
@@ -1,13 +1,23 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
+var mongoose = require("mongoose")
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var articlesRouter = require('./routes/articles');
+
+
+
+//connection to database
+mongoose.connect("mongodb://localhost/blog",
+{useNewUrlParser:true,useUnifiedTopology:true},
+(err)=>console.log(err ? err: "Connection true"))
 
 var app = express();
+
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -20,8 +30,13 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
+app.use('/articles', articlesRouter);
+app.get("/createArticle",(req,res)=>{
+  res.render("articleform")
+})
+app.get("/page",(req,res)=>{
+  res.render("page")
+})
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
--- block-BNaadl/blog/routes/articles.js
+++ block-BNaadl/blog/routes/articles.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var Article = require("../models/articleSchema")
+var Article = require("../models/Article")
 var router = express.Router();
 
 /* GET users listing. */
@@ -15,20 +15,52 @@ router.get('/:id',(req, res, next)=>{
   let id = req.params.id;
   Article.findById(id,(err,article)=>{
     if(err) return next(err)
-    console.log(articles)
+    console.log(article)
     res.render("singleArticle",{article:article})
   })
 });
 
-router.get("/new", (req, res)=>{
-  res.render("articleForm")
-});
-
-router.post("/",(req,res)=>{
+router.post("/",(req,res,next)=>{
   Article.create(req.body,(err,articles)=>{
+    if(err) return next(err)
+    console.log(articles)
+    res.redirect("/articles/")
+  })
+})
+
+router.get('/:id/edit',(req, res, next)=>{
+  let id = req.params.id;
+  Article.findById(id,(err,article)=>{
+    if(err) return next(err)
+    console.log(article)
+    res.render("updateArticle",{article:article})
+  })
+});
+router.post("/:id/update",(req,res,next)=>{
+  let id = req.params.id;
+  Article.findByIdAndUpdate(id,req.body,(err,article)=>{
+    if(err) return next(err)
+    res.redirect("/articles/"+id)
+  })
+})
+
+router.get("/:id/delete",(req,res,next)=>{
+  let id = req.params.id;
+  Article.findByIdAndDelete(id,(err,article)=>{
+    console.log(article)
     if(err) return next(err)
     res.redirect("/articles")
   })
 })
+
+router.get("/:id/likes",(req,res,next)=>{
+  let id = req.params.id;
+  Article.findByIdAndUpdate(id,{likes:{$inc : 1}},(err,article)=>{
+    console.log(article)
+    if(err) return next(err)
+    res.redirect("/articles/"+id)
+  })
+})
+
 
 module.exports = router;
